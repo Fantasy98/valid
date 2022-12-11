7c96cd901a6c0fc60a61b4f43aed4d204d55a207
yuning <yuning@neptune.eecs.kth.se>
2022-12-11 09:47:09 +0100

    sparse data success

--- 22-12-10_save_torch.py
+++ 22-12-10_save_torch.py
@@ -5,5 +5,5 @@ normalized=False
 y_plus=30
 save_types= ["train","test","validation"]
 for save_type in save_types:
-    root_path = "/home/yuning/thesis/tensor/"
+    root_path = "/storage3/yuning/thesis/tensor/"
     TF2Torch(root_path,y_plus,var,target,save_type,normalized=False)
