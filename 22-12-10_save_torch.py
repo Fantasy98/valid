dbc1ddd2ab686966f627ea4329519c13e0cc5897
yuning <yuningw@kth.se>
2022-12-11 05:01:38 +0100

    Gengerate tensor

--- 22-12-10_save_torch.py
+++ 22-12-10_save_torch.py
@@ -3,6 +3,7 @@ var=['u_vel',"v_vel","w_vel","pr0.025"]
 target=['pr0.025_flux']
 normalized=False
 y_plus=30
-save_type= "train"
-root_path = "/home/yuning/thesis/valid/tensor/"
-TF2Torch(root_path,y_plus,var,target,save_type,normalized=False)
+save_types= ["train","test","validation"]
+for save_type in save_types:
+    root_path = "/home/yuning/thesis/tensor/"
+    TF2Torch(root_path,y_plus,var,target,save_type,normalized=False)
